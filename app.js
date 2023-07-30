75b3599bc7246fc0127d1a17c3c31fd4989fd58e
dreamingfree9 <STU91898@ardenuniversity.ac.uk>
2023-07-31 00:38:56 +0100

    Your commit message

--- app.js
+++ app.js
@@ -1,20 +1,52 @@
-// Import required modules
 const express = require('express');
+const app = express();
 const path = require('path');
 
-// Create an instance of Express app
-const app = express();
-
-// Serve static files from the public directory
+// Set up static file serving from the "public" folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Define routes
+// Route to handle the home page
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Start the server
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+// Route to handle the portfolio page
+app.get('/portfolio', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'portfolio.html'));
 });
+
+// Route to handle the IT pathways page
+app.get('/pathways', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'pathways.html'));
+});
+
+// Route to handle the forum page
+app.get('/forum', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'forum.html'));
+});
+
+// Start the server on port 3000
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+// Your existing server setup and routes
+// ...
+
+// Function to show the sign-in form and hide the register form
+function showSignInForm() {
+    document.getElementById('signInForm').style.display = 'block';
+    document.getElementById('registerForm').style.display = 'none';
+  }
+  
+  // Function to show the register form and hide the sign-in form
+  function showRegisterForm() {
+    document.getElementById('signInForm').style.display = 'none';
+    document.getElementById('registerForm').style.display = 'block';
+  }
+  
+  // Event listeners for sign-in and register buttons
+  document.getElementById('signInButton').addEventListener('click', showSignInForm);
+  document.getElementById('registerButton').addEventListener('click', showRegisterForm);
+  
