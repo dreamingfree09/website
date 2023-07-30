be41e97805e2a6d80148c0e7841994e00643f5e0
dreamingfree9 <STU91898@ardenuniversity.ac.uk>
2023-07-30 21:14:31 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+// Import required modules
+const express = require('express');
+const path = require('path');
+
+// Create an instance of Express app
+const app = express();
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Define routes
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
